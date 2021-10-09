5f503352338f1163344653bd68b01331019c3740
RomanBresson <roman.bresson.5@gmail.com>
2021-10-09 22:36:57 +0200

    added a Session file for saving and loading json

---
+++ src/Session.py
@@ -0,0 +1,23 @@
+from Classes import *
+import json
+import os
+
+def save(session_name):
+    events_str = {i:Event.event_dict[i].__dict__ for i in Event.event_dict}
+    dirpath = f'data/{session_name}'
+    if not os.path.exists(dirpath):
+        os.makedirs(dirpath)
+    with open(dirpath+'/events.json', 'w') as eventfile:
+        json.dump(events_str, eventfile)
+
+def load(session_name):
+    dirpath = f'data/{session_name}'
+    with open(dirpath+'/events.json', 'r') as eventfile:
+        json_obj = json.load(eventfile)
+    print(json_obj['1'])
+
+A = Event(10,11)
+B = Event(15,12)
+Z = Timeline(A,B)
+save('coucou')
+load('coucou')
